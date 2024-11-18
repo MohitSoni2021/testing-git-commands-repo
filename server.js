d0d9838e31406df6f6324e4957bcd9f598284056
mohitsoni2024 <mohitsoni2006ms@gmail.com>
2024-11-18 11:04:37 +0530

    minor updates

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ app.get('/', (req, res)=>{
 	res.send("This is the home page of this site");
 })
 
+app.get('/about', (req, res)=>{
+	res.send("This is the about page of the website");
+})
 
 app.listen(3000, ()=>{
 	console.log("The server is working on port 3000");
